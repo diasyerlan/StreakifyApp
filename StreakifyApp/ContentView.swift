fcff54ffb7632d4dbb84000a0426d0476f30e15a
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-11-26 17:17:40 +0500

    Initial Commit

---
+++ StreakifyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreakifyApp
+//
+//  Created by Dias Yerlan on 26.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreakifyApp/StreakifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakifyAppApp.swift
+//  StreakifyApp
+//
+//  Created by Dias Yerlan on 26.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct StreakifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
